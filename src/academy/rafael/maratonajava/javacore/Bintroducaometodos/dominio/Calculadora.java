645234bc3bc9901d9c6cfd058466f5a46c919a05
Rafael <rafaelfda2ney@gmail,com>
2026-03-10 22:39:56 -0300

    desenvolvimento sobre parâmetros do tipo primitivo aula 05

--- src/academy/rafael/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/rafael/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -19,4 +19,12 @@ public class Calculadora {
         }
         return num01/num02;
     }
+
+    public void alteraDoisNumeros(int numero01, int numero02) {
+        numero01= 99;
+        numero02 = 22;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("Num01: " + numero01);
+        System.out.println("Num02: " + numero02);
+    }
 }
