b4502c1bf41be82f91e365127b3424d9cc0921cb
Rafael <rafaelfda2ney@gmail,com>
2026-03-17 22:59:03 -0300

    introdução sobre varargs aula 09

--- src/academy/rafael/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste02.java
+++ src/academy/rafael/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste02.java
@@ -1,10 +1,10 @@
 package academy.rafael.maratonajava.javacore.Bintroducaometodos.teste;
 
-import academy.rafael.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+import academy.rafael.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora01;
 
 public class CalculadoraTeste02 {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
+        Calculadora01 calculadora = new Calculadora01();
         calculadora.multiplicaDoisNumeros(10,20);
     }
 }
--- src/academy/rafael/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste03.java
+++ src/academy/rafael/maratonajava/javacore/Bintroducaometodos/teste/CalculadoraTeste03.java
@@ -1,10 +1,10 @@
 package academy.rafael.maratonajava.javacore.Bintroducaometodos.teste;
 
-import academy.rafael.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+import academy.rafael.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora01;
 
 public class CalculadoraTeste03 {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
+        Calculadora01 calculadora = new Calculadora01();
         int result = (int) calculadora.divideDoisNumeros(20, 2);
         System.out.println(result);
         System.out.println("-------------------------");
