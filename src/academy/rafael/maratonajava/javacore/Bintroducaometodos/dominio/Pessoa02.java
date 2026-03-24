e57f92157fed8de6807eab205cbc7df15312436d
Rafael <rafaelfda2ney@gmail,com>
2026-03-24 11:15:19 -0300

    desenvolvimento sobre modificadores de acesso private (get, set ) aula 12

---
+++ src/academy/rafael/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa02.java
@@ -0,0 +1,32 @@
+package academy.rafael.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Pessoa02 {
+    private String nome;
+    private int idade;
+
+    public void imprime(){
+        System.out.println(getNome());
+        System.out.println(getIdade());
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            System.out.println("idade invalida !");
+            return;
+        }
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+}
